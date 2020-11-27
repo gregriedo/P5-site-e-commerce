29845feec5be76d0bff9d982c929f7f3f9f2e5f8
Greg <gregory.riedo@gmail.com>
2020-11-27 14:18:27 +0100

    ajout page JS confirmation.js

---
+++ scripts/confirmation.js
@@ -0,0 +1,10 @@
+let totalPrice = localStorage.getItem("prixTotal");
+let orderId = localStorage.getItem("orderId");
+
+let userOrder = document.getElementById("order_id");
+userOrder.innerHTML = orderId;
+let somme = document.getElementById("order_total");
+somme.innerHTML= totalPrice;
+
+
+localStorage.clear();
