d08961a67c5239323e3ebc51a740792b8ba7293f
Greg <gregory.riedo@gmail.com>
2020-12-31 17:44:58 +0100

    modification code JS dans index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,42 @@
 let url ="http://localhost:3000/api/cameras";
 let main=document.getElementById("main");
+
+//Récupération des données pour afficher les caméras
 fetch(url)
-.then(
-  function(response) {
+.then( response=>{
     if (response.status !== 200) {
-      console.log('Looks like there was a problem. Status Code: ' +
+      console.log('Erreur avec le code: ' +
       response.status);
       return;
     }
+    response.json().then(data=>{
+      data.forEach(element =>{
+        showCamera(element);
 
-    // Examine the text in the response
-  response.json().then(function(data) {
-    console.log(data);
-    data.forEach(function(element){
-      let div=document.createElement("div");
-      div.classList='col-3';
-      let a=document.createElement("a");
-      a.href="produit.html?id="+element._id;
-      a.innerHTML=element.name;
-      let img=document.createElement("img");
-      img.src=element.imageUrl;
-      img.classList='w-100';
-      let p=document.createElement("p");
-      p.innerHTML=element.price + "€";
-      div.appendChild(a);
-      div.appendChild(img);
-      div.appendChild(p);
-      main.appendChild(div);
+      })
 
-    });
-  });
-}
-)
-.catch(function(err) {
-console.log('Fetch Error :-S', err);
+    })
+})
+
+.catch(err=> {
+  console.log('Message erreur', err);
 });
+
+//Fonction pour afficher les caméras
+function showCamera (element){
+  let div=document.createElement("div");
+  div.classList='col-md-3';
+  let a=document.createElement("a");
+  a.href="produit.html?id="+element._id;
+  a.innerHTML=element.name;
+  let img=document.createElement("img");
+  img.src=element.imageUrl;
+  img.classList='w-100';
+  let p=document.createElement("p");
+  p.innerHTML=element.price + "€";
+  div.appendChild(a);
+  div.appendChild(img);
+  div.appendChild(p);
+  main.appendChild(div);
+
+};
